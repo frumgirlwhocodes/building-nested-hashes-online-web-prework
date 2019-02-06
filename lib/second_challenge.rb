5aebdfbb06c8f25f8a8f031e633bf97392eb84a7
lieby weintraub <liebahinda@gmail.com>
2019-02-06 01:36:54 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,6 +1,6 @@
 def second_challenge
   epic_tragedy = {
-  :montague => {:patriarch =>{},:patriarch =>{},:hero =>{},
+  :montague => {:patriarch =>{},:patriarch =>{},:hero =>{},:hero_friends => 
       #your key/value pairs here!
       }, 
     :capulet => {
